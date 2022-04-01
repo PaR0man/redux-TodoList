03f16713a6150bef18a38204d207a6c42076ff06
PaR0man <pavelromanov.2000@gmail.com>
2022-04-01 19:12:23 +0300

    fix: remove broken imports and implement Header

---
+++ src/containers/Header/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <div className="headerRoot">
+      <h1>TODO_list</h1>
+      <h2>Created with React-Redux</h2>
+    </div>
+  );
+};
